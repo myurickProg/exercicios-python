7b1f014cec98f3873ad752da380f690e1f5729e5
Erick Silva <biladero528@gmail.com>
2024-09-11 12:53:39 -0300

    minor fix

--- exercicios/exercicioCondicionais.py
+++ exercicios/exercicioCondicionais.py
@@ -1,8 +1,20 @@
 produtos = ["rojão", "peido de veia", "palitinho"]
 precos = [5, 3, 1]
 
+novo_produto = input("Digite o nome do novo produto: ")
+novo_produto = novo_produto.lower()
+
+produtos.append(novo_produto)
+
+novo_preco = input("Digite o preco do produto: ")
+novo_preco = novo_preco.lower()
+
+precos.append(novo_preco)
+
+
 produto = input("Digite o nome do produto: ")
 produto = produto.lower()
+
 
 if produto in produtos:
     index_produto = produtos.index(produto)
